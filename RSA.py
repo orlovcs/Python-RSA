b5cd3ef8033b99d45f6684442c87945de4f32b8f
Nikita <thetax45@gmail.com>
2017-09-10 19:13:14 -0400

    Update RSA.py
    Nick did this

--- RSA.py
+++ RSA.py
@@ -20,34 +20,26 @@ def primeCheck(prime):
 def publickeys():
     p = input("Enter a large distinct prime p: ")
     q = input("Enter a large distinct prime q: ")
-
     p = int(p)
     q = int(q)
     while p==q or not primeCheck(p) or not primeCheck(q) or p <=100 or q <= 100:
-
       if p==q:
         print("Error: numbers are identical")
-        
-        
       if not primeCheck(p):
         print("Error: p is not a prime")
-
       if not primeCheck(q):
-        print("Error: q is not a prime")
-        
+        print("Error: q is not a prime") 
       if p <= 100:
         print("Error: p must be larger than 100")
-        
       if q <= 100: 
         print("Error: q must be larger than 100")
-        
-      
       p = input("Enter a large distinct prime p: ")
       p = int(p)
       q = input("Enter a large distinct prime q: ")
       q = int(q)
     n = p*q  
     phiN = phi(p,q)
+    
     print("Your p value is:",p)
     print("Your q value is:",q)    
     print("Your n value is:",n)
@@ -55,18 +47,13 @@ def publickeys():
     
     e = input("Enter a value greater than 1 and less than phi:")
     e = int(e)
-    
     while e >= phiN or e <= 1 or math.gcd(e,phiN) != 1:
       if e >= phiN:
         print("Error: e is too large")
-        
       if e <= 1: 
         print("Error: e is too small")
-        
       if  math.gcd(e,phiN) != 1:
         print("Error: e and phi have a non-one common denominator")
-        
-    
       e = input("Enter a value greater than 1 and less than phi:" )
       e = int(e)
     print("Your e value is:",e)
